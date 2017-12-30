b61f1b7d88b51daa83efb28bf4270c46a864a641
zefelise <zefelise@gmail.com>
2017-12-30 14:45:08 +0800

    Complete week 4 assignment

--- Week 4/Assigment/ex3/predict.m
+++ Week 4/Assigment/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,11 +21,12 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
+A1 = [ones(m, 1) X];
+Z2 = A1*Theta1';
+A2 = [ones(m, 1) sigmoid(Z2)];
+Z3 = A2*Theta2';
+H = sigmoid(Z3);
+[mval, p] = max(H,[],2);
 
 
 
