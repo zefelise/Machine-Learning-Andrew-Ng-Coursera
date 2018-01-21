16d4281da8417b845cc127663f55973553be3748
zefelise <zefelise@gmail.com>
2018-01-21 21:55:38 +0800

    Week 9, done.

--- Week 9/Assignment/ex8/cofiCostFunc.m
+++ Week 9/Assignment/ex8/cofiCostFunc.m
@@ -11,7 +11,7 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -21,7 +21,7 @@ Theta_grad = zeros(size(Theta));
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
+%               matches our costs. After that, you should implement the
 %               gradient and use the checkCostFunction routine to check
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
@@ -29,29 +29,28 @@ Theta_grad = zeros(size(Theta));
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the
 %            i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
+%        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        Theta_grad - num_users x num_features matrix, containing the
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = sum(sum((X*Theta'.*R-Y).^2))/2;
+J_reg = 0.5*lambda*sum(sum(X.^2)) + 0.5*lambda*sum(sum(Theta.^2));
+J = J + J_reg;
 
+X_grad = (X*Theta'.*R-Y)*Theta;
+X_grad_reg = lambda * X;
+X_grad = X_grad + X_grad_reg;
 
-
-
-
-
-
-
-
-
-
-
+Theta_grad = (X*Theta'.*R-Y)'*X;
+Theta_grad_reg = lambda * Theta;
+Theta_grad = Theta_grad + Theta_grad_reg;
 
 
 
--- Week 9/Assignment/ex8/selectThreshold.m
+++ Week 9/Assignment/ex8/selectThreshold.m
@@ -12,28 +12,27 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
+
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
     %               end of the loop will compare the F1 score for this
     %               choice of epsilon and set it to be the best epsilon if
     %               it is better than the current choice of epsilon.
-    %               
+    %
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
 
-
-
-
-
-
-
-
-
-
-
+    cvPredictions = pval<epsilon;
+    tp = sum(yval & cvPredictions);
+    fp = sum(xor(yval, cvPredictions) & cvPredictions);
+    %fp = sum((cvPredictions == 1) & (yval == 0));
+    fn = sum(xor(yval, cvPredictions) & yval);
+    %fn = sum((cvPredictions == 0) & (yval == 1));
+    p = tp/(tp+fp);
+    r = tp/(tp+fn);
+    F1 = 2*p*r/(p+r);
 
     % =============================================================
 
